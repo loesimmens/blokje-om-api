3c39ed0ca105fdb1592511c634fbe5084f205268
Loes Immens <loes.immens@alliander.com>
2025-06-08 13:21:26 +0200

    some upgrades

--- src/test/kotlin/nl/blokjeom/blokjeomapi/boardgames/services/BoardGameGeekApiServiceTest.kt
+++ src/test/kotlin/nl/blokjeom/blokjeomapi/boardgames/services/BoardGameGeekApiServiceTest.kt
@@ -1,19 +1,19 @@
 package nl.blokjeom.blokjeomapi.boardgames.services
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import nl.blokjeom.blokjeomapi.boardgames.TestHelper.getFileAsString
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BoardGameGeekApiServiceTest {
     private val service = BoardGameGeekApiService()
-
-    private val logger = KotlinLogging.logger {}
 
     @Test
     fun mapXml() {
         val xml = getFileAsString("boardgame.xml")
 
         val result = service.mapXml(xml)
+
+        assertThat(result.boardgame.first()).isNotNull
     }
 
 }
