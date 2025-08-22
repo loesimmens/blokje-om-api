9d726cfcce6290f106f1c78ad431b73801f1486b
Loes Immens <loes.immens@alliander.com>
2025-08-22 16:11:27 +0200

    not comparing time

--- src/test/kotlin/nl/blokjeom/blokjeomapi/orders/services/OrderServiceTest.kt
+++ src/test/kotlin/nl/blokjeom/blokjeomapi/orders/services/OrderServiceTest.kt
@@ -69,6 +69,8 @@ class OrderServiceTest {
         val result = orderService.order(order)
 
         assertThat(result).isEqualTo(order)
+            .usingRecursiveComparison()
+            .ignoringFields("pickUpTime")
 
         verifySequence {
             mailService.sendMessageUsingThymeleafTemplate(
