3fd70f9f35f9e519e1c32b4014ff574ae6c26970
Loes Immens <loes.immens@alliander.com>
2025-12-30 17:09:54 +0100

    readme, dev env config, db host config

--- src/main/kotlin/nl/blokjeom/blokjeomapi/application/config/DatabaseConfig.kt
+++ src/main/kotlin/nl/blokjeom/blokjeomapi/application/config/DatabaseConfig.kt
@@ -1,5 +1,6 @@
 package nl.blokjeom.blokjeomapi.application.config
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import nl.blokjeom.blokjeomapi.application.helpers.Environment
 import nl.blokjeom.blokjeomapi.application.helpers.EnvironmentHelper
 import org.springframework.boot.jdbc.DataSourceBuilder
@@ -9,9 +10,11 @@ import javax.sql.DataSource
 
 @Configuration
 class DatabaseConfig {
+    private val logger = KotlinLogging.logger {  }
     @Bean
     fun getDataSource(databaseConfigurationProperties: DatabaseConfigurationProperties): DataSource {
         val dataSourceBuilder = DataSourceBuilder.create()
+        logger.debug { "Configuring DataSource with URL: ${databaseConfigurationProperties.url} and Username: ${databaseConfigurationProperties.username}" }
         dataSourceBuilder.url(databaseConfigurationProperties.url)
         dataSourceBuilder.username(databaseConfigurationProperties.username)
         dataSourceBuilder.password(EnvironmentHelper.getSecretFromFileInEnvVariable(Environment("POSTGRES_PASSWORD_FILE")))
